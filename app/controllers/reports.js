067226d3ce1a7cb4c9590fd5b22131d61ee87f8e
radzisze <radzisze@ualberta.ca>
2014-03-23 15:02:16 -0600

    progress on reports (buggy)

--- app/controllers/reports.js
+++ app/controllers/reports.js
@@ -1,8 +1,14 @@
 var db = require('orm').db,
   Person = db.models.persons;
 User = db.models.users;
+Record = db.models.radiology_record;
 
 exports.index = function(req, res){
-res.render('reports/index');
-};
+	Person.add(function(err,persons) {
+		Record.add(function(err,records) {
+			console.log(records);
+		res.render('reports/index',{persons:persons, radiology_record:records});	
+		});
+	});
 
+};
