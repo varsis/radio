6513c4ad95d49f1658797940b969e02c3998b9db
varsis <chris@varsisstudio.com>
2014-03-28 07:29:28 -0600

    new app

--- config/config.js
+++ config/config.js
@@ -29,9 +29,11 @@ var config = {
       name: 'radiologyinformationsystem'
     },
     port: 3000,
-  // db: 'mysql://checkte_391:pa$$word@varsisstudio.com/checkte_391'
-        db: 'mysql://test:test@localhost/rsi_dev'
+  // db: 'mysql://checkte_391:pa$$word@us-cdbr-azure-northcentral-a.cleardb.com/checkte_391'
+        db: 'mysql://b8e8de0ff6cbc7:50357e27@us-cdbr-azure-northcentral-a.cleardb.com/radio'
   }
 };
 
+
+//Database=radio;Data Source=us-cdbr-azure-northcentral-a.cleardb.com;User Id=b8e8de0ff6cbc7;Password=50357e27
 module.exports = config[env];
