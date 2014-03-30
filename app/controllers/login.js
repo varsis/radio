a19a21a6e85235a79d60753b85863acb493d89e0
radzisze <radzisze@ualberta.ca>
2014-03-30 17:30:45 -0600

    login comments/removed console logs

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,12 +1,18 @@
+/*
+login module: renders login/logout pages and handles sessions
+*/
+
 var db = require('orm').db,
  Person = db.models.persons;
 var User = db.models.users;
 var passport = require('passport');
 
+//renders login page
 exports.index = function(req, res){
         res.render('login/index');
 };
 
+//session handling and rendering logout
 exports.logout = function(req,res) {
     req.session.destroy();
     res.render('login/logout'); d
